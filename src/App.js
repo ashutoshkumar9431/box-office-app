a48dfbf5778e894c75adaa156bcdc835aa4941ba
ashutosh <ashutoshkumar99055@gmail.com>
2021-07-26 01:56:25 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-
+import {Switch,Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+  return <Switch>
+   <Route exact path = "/">
+    t his is 
+   </Route >
+   <Route exact path = "/page1">
+    this is  page1
+   </Route >
+   <Route exact path = "/page2">
+    this is  page2
+   </Route >
+   <Route >
+    eroor page not found 
+   </Route >
+  </Switch>
+  
+  
 }
 
 export default App;
