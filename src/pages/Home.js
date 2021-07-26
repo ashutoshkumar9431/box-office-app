cab5c951c25faad4fd7e860738c2ccda6bf85693
ashutosh <ashutoshkumar99055@gmail.com>
2021-07-26 22:07:30 +0530

    displayed small results and small fetch refractor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,15 @@
 import React ,{useState}from 'react'
 import { MainPageLayout } from '../components/MainPageLayout'
+import  {apiGet}  from '../misc/config'
 
 export const Home = () => {
   const[input,setInput] = useState('')
+  const[results,setResult] = useState(null)
 
   const onClickChange = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResult(result);
+    });
   };
   const newInput = (ev) =>
   { setInput (ev.target.value)}
@@ -19,11 +19,24 @@ export const Home = () => {
       if(ev.keyCode===13)
       onClickChange()
   }
-  
+  const renderResult = () =>
+  { 
+     if(results && results.length===0)
+     {
+        return <div>No results</div>
+     }
+     if(results && results.length>0)
+     {
+      return <div>{results.map( (items) => <div key = {items.show.id}>{items.show.name}</div>)}</div>
+     }
+   return null
+
+  }
   return (
    <MainPageLayout>
        <input type = "text" onChange = {newInput}  onKeyDown ={onEnter} value = {input} />
        <button type = "button" onClick = {onClickChange}>Search</button>
+       {renderResult()}
       </MainPageLayout>
   )
 }
